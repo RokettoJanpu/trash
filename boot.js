885b1c9ed6ddc14ee8f769843f3c44be0b89676a
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 01:50:17 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -5,6 +5,12 @@ const client = new Discord.Client();
 var giphyAPIClient = require(`giphy-js-sdk-core`);
 giphy = giphyAPIClient(process.env.GIPHY_KEY);
  
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync(`./bot_commands`).filter(file => file.endsWith(`.js`));
+for (const file of commandFiles) {
+	const command = require(`./bot_commands/${file}`);
+	bot.commands.set(command.name, command);
+}
 
 client.on(`ready`, () => {
     console.log(`I am ready!`);
